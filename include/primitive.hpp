1f3b3bf0d15574b14c944e335e9e3d1e96a8fd97
Niko Salakka <nsalakka@gmail.com>
2023-12-16 18:17:58 +0200

    5: Drawing balls

---
+++ include/primitive.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Primitive {
+protected:
+public:
+    virtual bool Hit(glm::fvec3 ray_position, glm::fvec3 ray_direction) {}
+    glm::fvec3 get_position() { return this->position; }
+    glm::fvec3 get_color() { return this->color; }
+
+protected:
+    glm::fvec3 position = glm::fvec3(0.0f);
+    glm::fvec3 color = glm::fvec3(1.0f, 0.0f, 0.5f);
+};
+
+class Sphere : public Primitive {
+public:
+    Sphere() : Primitive() {}
+    Sphere(float radius) { this->radius = radius; }
+    Sphere(glm::fvec3 position, glm::fvec3 color, float radius) {
+        this->position = position;
+        this->color = color;
+        this->radius = radius;
+    }
+
+    bool Hit(glm::fvec3 ray_position, glm::fvec3 ray_direction) {
+        glm::fvec3 oc = ray_position - this->position;
+        auto a = glm::dot(ray_direction, ray_direction);
+        auto b = 2.0f * glm::dot(oc, ray_direction);
+        auto c = glm::dot(oc, oc) - this->radius*this->radius;
+        auto discriminant = b*b - 4*a*c;
+        return discriminant >= 0;
+    }
+
+    void set_radius(float radius) { this->radius = radius; }
+    
+private:
+    float radius = 1.0f;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 #include <raylib.h>
 
 #include "ray.hpp"
+#include "primitive.hpp"
 
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 1024;
 const float ASPECT_RATIO = SCREEN_WIDTH / SCREEN_HEIGHT;
+Sphere sphere = Sphere(glm::fvec3(0.0f, 0.0f, -1.0f), glm::fvec3(1.0f, 0.0f, 0.5f), 0.5f);
 
 
 int c_f2i(const float f) { return (f >= 1.0 ? 255 : (f <= 0.0 ? 0 : (int)floor(f * 256.0))); }
@@ -24,6 +26,11 @@ Color color_fvec(const glm::fvec3 v) {
 
 
 Color ray_color_at(const Raybaser::Ray& ray) {
+
+    if(sphere.Hit(ray.get_position_at(1.0f), ray.get_direction())) {
+        return color_fvec(sphere.get_color());
+    }
+
     glm::fvec3 unit_direction = glm::normalize(ray.get_direction());
     auto a = 0.5f * (unit_direction.y + 1.0f);
 
@@ -47,10 +54,11 @@ void raytrace(Image* output) {
     auto viewport_upper_left = camera_position - glm::fvec3(0.0f, 0.0f, focal_length) - u/2.0f - v/2.0f;
     auto first_pixel = viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
 
+    #pragma omp parallel for
     for (int y = output->height-1; y >= 0; y--) {
         for (int x = 0; x < output->width; x++) {
-            glm::fvec3 pixel_center = first_pixel 
-                + (static_cast<float>(x) * pixel_delta_u) 
+            glm::fvec3 pixel_center = first_pixel
+                + (static_cast<float>(x) * pixel_delta_u)
                 + (static_cast<float>(y) * pixel_delta_v);
             glm::fvec3 ray_direction = pixel_center - camera_position;
             Raybaser::Ray r(camera_position, ray_direction);
